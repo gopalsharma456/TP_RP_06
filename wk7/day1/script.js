02e7d27ffcc8302ae52bd2d8a66eeea631955020
gopal <gopalsharma456@gmail.com>
2024-03-26 22:35:35 +0530

    Arrays and methods with exercise

---
+++ wk7/day1/script.js
@@ -0,0 +1,66 @@
+// let arr = ['arr1', 'arr2', 'arr3', 'arr4', 'arr5']
+// //print arr
+// console.log(arr)
+
+// //print length of arr
+// console.log(`Lenght of arr is ${arr.length}`)
+
+// //replace arr element
+// arr[0] = 'arr6'
+// console.log(arr)
+
+// //get last element
+// console.log(arr[arr.length - 1])
+
+// // problem solve - calculate age
+// let calAge = (birthYear) => {
+//     return 2024 - birthYear
+// }
+
+// let birthYears = [1999, 1991, 1990, 1992, 1989]
+
+// console.log(calAge(birthYears[2]))
+// console.log(calAge(birthYears[birthYears.length - 1]))
+
+// //Array methods
+
+// let arr2 = ['name1', 'name2', 'name3', 'name4', 'name5']
+
+// //add element to the array
+// arr2.push('name6')
+// console.log(arr2)
+
+// //remove element from the array
+// arr2.pop()
+// console.log(arr2)
+
+// //add element from left or start of the array
+// arr2.unshift('name0')
+// console.log(arr2)
+
+// //remove element from the start
+// arr2.shift()
+// console.log(arr2)
+
+// //To find out the index of element
+// console.log(arr2.indexOf('name4'))
+
+// //To find out the element is present in the array
+// //used in condition checking(if else, switch etc...)
+// console.log(arr2.includes('name7'))
+
+// Exercise - Tip calculator using array
+
+// let bills = [];
+
+// function calcTip(bill) {
+//   bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 0.2);
+//   let total = bill + tip;
+//   return bills.push(total);
+// }
+
+// calcTip(100);
+// calcTip(10);
+// calcTip(400);
+
+// console.log(bills);
