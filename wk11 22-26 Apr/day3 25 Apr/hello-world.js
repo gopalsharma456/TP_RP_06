897dcdf5fd5de1005ac4b4bb1625709ddda60c1d
gopal <gopalsharma456@gmail.com>
2024-04-26 19:26:41 +0530

    area function updated

--- wk11 22-26 Apr/day3 25 Apr/hello-world.js
+++ wk11 22-26 Apr/day3 25 Apr/hello-world.js
@@ -1,54 +1,46 @@
-console.log('hello-world')
-console.log('hello-world')
-console.log('hello-world')
-console.log('hello-world')
-console.log('hello-world')
+console.log("hello-world");
+console.log("hello-world");
+console.log("hello-world");
+console.log("hello-world");
+console.log("hello-world");
 
-console.log(5+2)
-console.log(5+1)
-console.log(5+3)
-console.log(5+4)
-console.log(5+6)
-
+console.log(5 + 2);
+console.log(5 + 1);
+console.log(5 + 3);
+console.log(5 + 4);
+console.log(5 + 6);
 
 // Print Name dynamically in string
-function printName(name){
-    console.log('Hello, "' + name + '"') // with string concatenation
-    console.log(`Hello, "${name}"`) // with template literals
+function printName(name) {
+  console.log('Hello, "' + name + '"'); // with string concatenation
+  console.log(`Hello, "${name}"`); // with template literals
 }
 
-printName('James') // calling printName function
-
+printName("James"); // calling printName function
 
 //Calculate Area of Rectangle
-function areaOfRectangle(length, breadth){
-    return length * breadth
+function areaOfRectangle(length, breadth) {
+  let area = console.log("Area of Rectangle is:", length * breadth);
+  return area;
 }
 
-console.log('Area of Rectangle is:',areaOfRectangle(5, 10))
-
+areaOfRectangle(5, 10);
 
 // Celsius to Fahrenheit conversion
-function cToF(celsius){
-    let fahrenheitTemperature = (9 / 5) * celsius + 32;
-    return `${celsius}°C = ${fahrenheitTemperature.toFixed(2)}°F`
+function cToF(celsius) {
+  let fahrenheitTemperature = (9 / 5) * celsius + 32;
+  return `${celsius}°C = ${fahrenheitTemperature.toFixed(2)}°F`;
 }
 
-console.log(cToF(44))
-
+console.log(cToF(44));
 
 // Fahrenheit to Celsius conversion
-function fToC(fahrenheit){
-    let celsiusTemperature = (fahrenheit - 32) * (5 / 9);
-    return `${fahrenheit}°F = ${celsiusTemperature.toFixed(2)}°C`
+function fToC(fahrenheit) {
+  let celsiusTemperature = (fahrenheit - 32) * (5 / 9);
+  return `${fahrenheit}°F = ${celsiusTemperature.toFixed(2)}°C`;
 }
 
-console.log(fToC(99))
-
-
-
-
-
+console.log(fToC(99));
 
 //---->>>> String Methods <<<----
 
@@ -60,11 +52,10 @@ console.log(fToC(99))
 // at()                     - returns character at specified index in parenthesis introduced in ES2022
 
 // charCodeAt()             - returns character "code" at specified index in parenthesis ===> charCode for h is 72
-// [ ]                      - property access method 
+// [ ]                      - property access method
 
-    // difference between CharacterData() & [] property access method
-        // - If no character is found, [ ] returns """undefined""", while charAt() returns an """empty string""". 
-
+// difference between CharacterData() & [] property access method
+// - If no character is found, [ ] returns """undefined""", while charAt() returns an """empty string""".
 
 // :::: Extracting the part of the string in defined range ::::
 
@@ -74,13 +65,12 @@ console.log(fToC(99))
 
 // :::: Notes ::::
 
-        // - start or end can be positive or negative 
-        // - start position is compulsory while end can be left blank(will go till the last position)
+// - start or end can be positive or negative
+// - start position is compulsory while end can be left blank(will go till the last position)
 
-        // - in substring start end end values less than 0(negative) are considered as 0
+// - in substring start end end values less than 0(negative) are considered as 0
 
-        // - in substr second parameter is length of string to be extracted (not end position like slice and substring)
-
+// - in substr second parameter is length of string to be extracted (not end position like slice and substring)
 
 // ::: manipulation on string :::
 
@@ -94,32 +84,32 @@ console.log(fToC(99))
 
 // repeat()                 - repeats string by defined number  ("string1".repeat(2) --> string1string1)
 
-// replace()                - replaces the "first" occurrence of the matched string in string set 
-                            // example
-                                // - let text = "Please visit Microsoft!";
-                                // - let newText = text.replace("MICROSOFT", "W3Schools");
+// replace()                - replaces the "first" occurrence of the matched string in string set
+// example
+// - let text = "Please visit Microsoft!";
+// - let newText = text.replace("MICROSOFT", "W3Schools");
 
-                            // for replace all the occurrences of string use REGEX with /g(global match)
-                                // let text = "Please visit Microsoft and Microsoft!";
-                                // let newText = text.replace(/Microsoft/g, "W3Schools");
+// for replace all the occurrences of string use REGEX with /g(global match)
+// let text = "Please visit Microsoft and Microsoft!";
+// let newText = text.replace(/Microsoft/g, "W3Schools");
 
 // replaceAll()             - replaces the "ALL" occurrence of the matched "REGEX pattern" in string set
-                            // example
-                                // - text = text.replaceAll("Cats","Dogs");
-                                // - text = text.replaceAll("cats","dogs");
+// example
+// - text = text.replaceAll("Cats","Dogs");
+// - text = text.replaceAll("cats","dogs");
 
-                            // using REGEX 
-                            // text = text.replaceAll(/Cats/g,"Dogs");
-                            // text = text.replaceAll(/cats/g,"dogs");
+// using REGEX
+// text = text.replaceAll(/Cats/g,"Dogs");
+// text = text.replaceAll(/cats/g,"dogs");
 
-                // ::::: NOTE ::::
-                    // - replace() & replaceAll() are case sensitive
-                    // - use REGEX /i to make in case insensitive
-                    // - if used REGEX use /g in replaceAll() to avoid type error
+// ::::: NOTE ::::
+// - replace() & replaceAll() are case sensitive
+// - use REGEX /i to make in case insensitive
+// - if used REGEX use /g in replaceAll() to avoid type error
 
 // split()                  - splits string on defined character or symbol and returns array
-                        // example
-                            // text.split("")    // Splits & gives single characters in array --> 'wow'.split('') -->> ['w', 'o', 'w'] 
-                            // text.split(",")    // Split on commas
-                            // text.split(" ")    // Split on spaces
-                            // text.split("|")    // Split on pipe
+// example
+// text.split("")    // Splits & gives single characters in array --> 'wow'.split('') -->> ['w', 'o', 'w']
+// text.split(",")    // Split on commas
+// text.split(" ")    // Split on spaces
+// text.split("|")    // Split on pipe
