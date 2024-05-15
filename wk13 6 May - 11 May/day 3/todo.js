69b2e66f2e3278c8c71cb774cb62e29a0e3ce3aa
gopal <gopalsharma456@gmail.com>
2024-05-15 15:07:18 +0530

    task count added

--- wk13 6 May - 11 May/day 3/todo.js
+++ wk13 6 May - 11 May/day 3/todo.js
@@ -1,29 +1,80 @@
-const inputBox = document.getElementById("input-box");
-const addButton = document.getElementById("addBtn");
-const listItems = document.getElementById("list-items");
-let id = 0;
+let inputBox = document.getElementById("input-box");
+let addButton = document.getElementById("addBtn");
+let listItems = document.getElementById("list-items");
+let taskCountDiv = document.getElementById("taskCount");
 
-addButton.addEventListener("click", function () {
+let taskId = 0;
+let completedTaskCount = 0;
+let totalTaskCount = 0;
+
+function taskCountDivDisplay() {
+  if (totalTaskCount == 0) {
+    taskCountDiv.innerText = "No Task Added";
+    taskCountDiv.style.color = "Green";
+  } else if (totalTaskCount == 1) {
+    taskCountDiv.innerText = `${completedTaskCount} Completed of Total ${totalTaskCount} Task`;
+    if (completedTaskCount == 1) {
+      taskCountDiv.style.color = "green";
+    } else {
+      taskCountDiv.style.color = "orange";
+    }
+  } else {
+    taskCountDiv.innerText = `${completedTaskCount} Completed of Total ${totalTaskCount} Tasks`;
+    if (completedTaskCount == totalTaskCount) {
+      taskCountDiv.style.color = "green";
+    } else {
+      taskCountDiv.style.color = "orange";
+    }
+  }
+}
+
+inputBox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTaskBtn();
+  }
+});
+
+function addTaskBtn() {
   if (inputBox.value == "") {
     alert("Please type something!!!");
   } else {
     listItems.innerHTML += `
-                            <li>${inputBox.value}
-                            <span>Delete</span>
+                            <li id='task-${taskId}'>${inputBox.value}
+                            <span id='completedTaskBtn' onclick="completedTask('task-${taskId}')">Completed</span>
+                            <span onclick="deleteTask('task-${taskId}')">Delete</span>
                             </li>
                             `;
+    taskId++;
+    totalTaskCount++;
+    taskCountDivDisplay();
   }
   inputBox.value = "";
-});
+  inputBox.focus();
+}
 
-
-
-
-listItems.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
+function completedTask(id) {
+  let liItem = document.getElementById(id);
+  if (liItem.style.textDecoration == "line-through") {
+    liItem.style.textDecoration = "none";
+    completedTaskCount--;
+    taskCountDivDisplay();
+  } else {
+    liItem.style.textDecoration = "line-through";
+    completedTaskCount++;
+    taskCountDivDisplay();
   }
-  if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
+}
+
+function deleteTask(id) {
+  let liItem = document.getElementById(id);
+  if (liItem.style.textDecoration == "line-through") {
+    completedTaskCount--;
+    totalTaskCount--;
+    taskCountDivDisplay();
+    liItem.remove();
+  } else {
+    totalTaskCount--;
+    taskCountDivDisplay();
+    liItem.remove();
   }
-});
+}
