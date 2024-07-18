223d58c664afadbb963cbea7fce45e11bb4ec85e
gopal <gopalsharma456@gmail.com>
2024-07-18 23:31:10 +0530

    oop concepts

--- intermediate/oops.js
+++ intermediate/oops.js
@@ -1,29 +1,76 @@
- class Student{
-    
-    constructor(first_name, last_name, contact, batch_code, tech){
-        this.first_name = first_name,
-        this.last_name = last_name,
-        this.contact = contact,
-        this.batch_code = batch_code,
-        this.tech = tech
-    }
-    updateContact(contact){
-        this.contact = contact
-    }
-    updateTech(tech){
-        this.tech.push(tech)
-    }
- }
+// what is oop
+// object
+// is JS -> Fully Object Oriented -> No
+// class, constructor, new, this
 
- const ted = new Student('Ted', 'Mosby', '4345345', 'TP_RP_06', ['HTML', 'CSS', 'JS'])
- const barney = new Student('Barney', 'Steinson', '453543453', 'TP_RP_06', ['HTML', 'CSS', 'JS'])
+// template & blueprint of an object
+// class Student {
 
- console.log(ted)
- console.log(barney)
- console.log('----------- After update')
- 
- ted.updateContact('123456')
- barney.updateTech('communication')
+//     // automatically call on creating an object
+//     constructor(first_name, last_name, mobile, batch_code, tech) {
+//         // current object
+//         this.name =  first_name + " " + last_name;
+//         this.mobile = mobile;
+//         this.tech = tech;
+//     }
 
- console.log(ted)
- console.log(barney)
+//     updateMobile(mobile) {
+//        this.mobile =  mobile;
+//     }
+
+//     addTech(tech) {
+//         this.tech.push(tech);
+//     }
+// }
+
+// // Object Creating
+// const shadab = new Student("Shadab", "Ansari", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
+// const sheela = new Student("Sheela", "Mishra", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
+
+// shadab.addTech("ReactJS")
+
+// console.log(shadab);
+
+class Account {
+    #balance;
+    constructor(id, name, balance) {
+      this.id = id;
+      this.name = name;
+      this.#balance = balance;
+    }
+  
+    depositAmount(amount) {
+      this.#balance += amount;
+      return this.#balance;
+    }
+  
+    withdrawAmount(amount) {
+      this.#balance -= amount;
+      return this.#balance;
+    }
+  
+    getBalance() {
+      return this.#balance;
+    }
+  }
+  
+  class CurrentAccount extends Account{
+    calculateInterest() {
+      return 0
+    }
+  }
+  
+  class SavingAccount extends Account {
+    calculateInterest() {
+      return this.getBalance() * 2.5 / 100; 
+    }
+  }
+  
+  const samruddhiAccount = new CurrentAccount(123, "Samruddhi", 0);
+  const sarojAccount = new SavingAccount(123, "Saroj", 1);
+  
+  sarojAccount.depositAmount(10000);
+  samruddhiAccount.depositAmount(2000);
+  
+  console.log(samruddhiAccount.calculateInterest());
+  console.log(sarojAccount.calculateInterest());
